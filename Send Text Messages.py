5c31da96893b7458428fdeadeb73951ee9c07682
Jaisidh Singh <75247817+jaisidhsingh@users.noreply.github.com>
2020-12-11 15:16:02 +0530

    Add files via upload
    Uses the popular API for text messages : Twilio. Note : texting media only available in the US :( . Cheers.

---
+++ Send Text Messages.py
@@ -0,0 +1,14 @@
+from twilio.rest  import Client
+
+client = Client(account_sid, auth_token)
+
+try:
+    client.messages.create(
+        to=f"+91{number1}",
+        from_= f"{number2}",
+        body="You son of a bitch you did it"
+    )
+except:
+    print("error")
+finally:
+    print("done")
